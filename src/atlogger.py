6740d32bf6b5a97c369acbb61623480e90e5d0fa
guilherme <thedrawnator@gmail.com>
2026-09-12 20:08:04 -0300

    feat: adding custom mlflow and wandb loggers

--- src/atlogger.py
+++ src/atlogger.py
@@ -1,14 +1,44 @@
-## documentation
+## uses decorator to create a logger that does not clutter the middle of the algorithms
 ## atloger
 ## ~ Guilherme Toledo 2026-09-12
 
-def log(fn):
+import logging 
+import logging.config
+from dataclasses import dataclass
+import json
+
+def log(fn,level=logging.INFO):
   # do something when importing fn with decorator
-  print(f"logging: {fn.__module__}:{fn.__name__}")
   def wrapper(*args, **kwargs):
     # do something before calling fn
     output = fn(*args,**kwargs)
-    # do something after calling fn
-    print(output)
+    match level:
+      case logging.DEBUG:
+        ATLOGGER.debug(output)
+      case logging.INFO:
+        ATLOGGER.info(output)
+      case logging.WARN:
+        ATLOGGER.warn(output)
+      case logging.ERROR:
+        ATLOGGER.error(output)
+      case logging.CRITICAL:
+        ATLOGGER.critical(output)
     return output
   return wrapper
+
+class MlflowHandler(logging.Handler):
+  def __init__(self,*args,**kwargs):
+    logging.Handler.__init__(self)
+  def emit(self,record):
+    pass
+
+class WandbHandler(logging.Handler):
+  def __init__(self,*args,**kwargs):
+    logging.Handler.__init__(self)
+  def emit(self,record):
+    pass
+
+with open("src/sample_config.json","r") as config_file:
+  config = json.load(config_file)
+logging.config.dictConfig(config)
+ATLOGGER = logging.getLogger(f"AtLogger")
